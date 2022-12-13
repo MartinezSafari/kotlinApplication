58f4120fc1bada4ef8329c898e93278093859044
martinez <martinez@thereachtrust.org>
2022-12-13 13:55:45 +0200

    add Calender + test

--- app/src/main/java/org/thereachtrust/kotlinapplication/NextActivity.kt
+++ app/src/main/java/org/thereachtrust/kotlinapplication/NextActivity.kt
@@ -2,12 +2,21 @@ package org.thereachtrust.kotlinapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_next.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
+
+        calendarView.setOnDateChangeListener { calendarView, year, month, day ->
+            val cal: Calendar = GregorianCalendar (year, month, day)
+            val dateFormatter = SimpleDateFormat ("yyyy/MM/dd")
+            Toast.makeText(this, dateFormatter.format(cal.time), Toast.LENGTH_LONG).show()
+        }
 
         doneButton.setOnClickListener{
             finish()
